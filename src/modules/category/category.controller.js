bba053425412b061b0736206d2011b78af8a275e
oraib26 <oraib.sleet@gmail.com>
2024-07-06 13:14:52 +0300

    ..

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -1,100 +1,75 @@
 import slugify from "slugify";
-import cloudinary from "../../utls/cloudinary.js";
 import categoryModel from "../../../DB/model/category.model.js";
+import cloudinary from "../../utls/cloudinary.js";
+import { pagination } from "../../utls/pagination.js";
+import { AppError } from "../../utls/AppError.js";
 
 
-export const create = async (req, res) => {
-  req.body.categoryName = req.body.categoryName.toLowerCase();
-
-  if (await categoryModel.findOne({ categoryName: req.body.categoryName })) {
-    return res.status(409).json({ message: "Category is already exists" });
-  }
-  req.body.slug = slugify(req.body.categoryName);
-
-  const { secure_url, public_id } = await cloudinary.uploader.upload(
-    req.file.path,
-    {
-      folder: `${process.env.STORENAME}/categories`,
+export const createCategories = async (req, res, next) => {
+    const name = req.body.name.toLowerCase();
+    if (await categoryModel.findOne({ name })) {
+        
+        return next(new AppError(`category name already exists`, 409 ));
     }
-  );
+    const { secure_url, public_id } = await cloudinary.uploader.upload(req.file.path,
+        { folder: `${process.env.APP_NAME}/categories` });
 
-  req.body.image = { secure_url, public_id };
+    const category = await categoryModel.create({ name, slug: slugify(name), image: { secure_url, public_id },
+    createdBy: req.user._id,
+    updatedBy: req.user._id, });
+    return res.status(201).json({ message: 'success', category });
 
-  req.body.createdBy = req.user._id;
-  req.body.updatedBy = req.user._id;
-  const category = await categoryModel.create(req.body);
-
-  return res.json({ message: category });
-};
-
-export const getAll = async (req, res) => {
-  const categories = await categoryModel.find({}).populate({
-    path: "subcategory",
-  });
-  return res.status(200).json({ message: "success", categories });
-};
-
-export const getActive = async (req, res) => {
-  const categoriesActive = await categoryModel
-    .find({ status: "active" })
-    .select("categoryName");
-  return res.status(200).json({ message: "success", categoriesActive });
-};
-
-export const getDetails = async (req, res) => {
-  const { id } = req.params;
-  const category = await categoryModel.findById({ _id: id });
-  return res.status(200).json({ message: "success", category });
-};
-
-export const update = async (req, res) => {
-  const category = await categoryModel.findById(req.params.id);
-
-  if (!category) {
-    return res.status(404).json({ message: "category not found" });
-  }
-  //return res.json(req.body);
-
-  category.categoryName = req.body.categoryName.toLowerCase();
-
-  if (
-    await categoryModel.findOne({
-      categoryName: req.body.categoryName,
-      _id: { $ne: req.params.id },
-    })
-  ) {
-    return res.status(409).json({ message: "category already exists" });
-  }
-  //فوق فحصنا انه اسم الكاتيجوري الجديد مش موجود بالداتابيس قبل
-
-  category.slug = slugify(req.body.categoryName);
-
-  if (req.file) {
-    const { secure_url, public_id } = await cloudinary.uploader.upload(
-      req.file.path,
-      {
-        folder: `${process.env.STORENAME}/categories`,
-      }
-    );
+}
+export const getAllCategories = async (req, res) => {
+    const { skip, limit } = pagination(req.query.page, req.query.limit);
+    const category = await categoryModel.find({}).skip(skip)
+    .limit(limit).populate('subcategory');
+    return res.status(200).json({ message: "success", category });
+}
+export const getActiveCategories = async (req, res) => {
+    const { skip, limit } = pagination(req.query.page, req.query.limit);
+    const category = await categoryModel.find({ status: 'Active' }).skip(skip)
+    .limit(limit).select('name image');
+    return res.status(200).json({ message: "success", category });
+}
+export const getDetailsCategories = async (req, res,next) => {
+    const category = await categoryModel.findById(req.params.id);
+    if(!category){
+    return next(new AppError(`category not found`,  404 ));
+    }
+    return res.status(200).json({ message: "success", category });
+}
+export const updateCategories=async(req,res,next)=>{
+     const category=await categoryModel.findById(req.params.id);
+     if(!category){
+        return next(new AppError(`invalid category id ${req.params.id}`,  404 ));
+      
+     }
+    category.name = req.body.name.toLowerCase();
+    if(await categoryModel.findOne({name:req.body.name,_id:{$ne:req.params.id}})){
+        return next(new AppError(`category ${req.body.name} already exists`,  409 ),
+          );
+    }
+    category.slug = slugify(req.body.name);
+    category.status=req.body.status;
+    if(req.file){
+     const { secure_url, public_id } = await cloudinary.uploader.upload(
+            req.file.path, { folder: `${process.env.APP_NAME}/categories` },
+          );
+          await cloudinary.uploader.destroy(category.image.public_id);
+          category.image = { secure_url, public_id };
+        }
+        category.updatedBy = req.user._id;
+await category.save();
+  return res.status(200).json({ message: 'success' });
+    }
+export const deleteCategories=async(req,res,next)=>{
+    const category=await categoryModel.findByIdAndDelete(req.params.id);
+    if(!category){
+     return next(new AppError(`categroy not found`, 404 ));
+    }
     await cloudinary.uploader.destroy(category.image.public_id);
-    category.image = { secure_url, public_id };
+  return res.status(200).json({ message: 'success' });
+}
 
-    category.status = req.body.status;
-    category.updatedBy = req.user._id;
 
-    await category.save();
-
-    return res.json({ message: "success", category });
-  }
-};
-
-export const destroy = async (req, res) => {
-  const category = await categoryModel.findByIdAndDelete(req.params.id);
-
-  if (!category) {
-    return res.status(404).json({ message: "category not found" });
-  }
-  await cloudinary.uploader.destroy(category.image.public_id);
-
-  return res.status(200).json({ mesaage: "success", category });
-};
