5ffa7d172a4dd016a1470dd72c14102edf82d037
oraib26 <oraib.sleet@gmail.com>
2024-07-08 20:19:20 +0300

    ..

--- db/model/category.model.js
+++ db/model/category.model.js
@@ -1,39 +1,37 @@
-import mongoose, { Schema, model, Types } from "mongoose";
-const categorySchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
+import mongoose,{Schema,model,Types} from 'mongoose' 
+const categorySchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
     },
-    slug: {
-      type: String,
-      required: true,
+    slug:{
+        type:String,
+        required:true,
     },
-    image: {
-      type: Object,
-      required: true,
+    image:{
+        type:Object,
+        required:true,
     },
-    status: {
-      type: String,
-      default: "Active",
-      enum: ["Active", "Inactive"],
+    status:{
+        type:String,
+        default:'Active',
+        enum:['Active','Inactive'],
     },
-    createdBy: { type: Types.ObjectId, ref: "User" },
-    updatedBy: { type: Types.ObjectId, ref: "User" },
-  },
-  {
-    timestamps: true,
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
-categorySchema.virtual("subcategory", {
-  localField: "_id",
-  foreignField: "categoryId",
-  ref: "Subcategory",
+    createdBy:{type:Types.ObjectId,ref:'User'},
+    updatedBy:{type:Types.ObjectId,ref:'User'},
+},
+{
+    timestamps:true,
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true},
+  
 });
+categorySchema.virtual('subcategory',{
+    localField:'_id',
+    foreignField:'categoryId',
+    ref:'Subcategory'
+})
 
-const categoryModel =
-  mongoose.models.Category || model("Category", categorySchema);
+const categoryModel = mongoose.models.Category ||  model('Category',categorySchema);
 export default categoryModel;
--- db/model/subcategory.model.js
+++ db/model/subcategory.model.js
@@ -1,32 +1,29 @@
-import mongoose, { Schema, model, Types } from "mongoose";
-const subcategorySchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
+import mongoose,{Schema,model,Types} from 'mongoose' 
+const subcategorySchema = new Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true,
     },
-    slug: {
-      type: String,
-      required: true,
+    slug:{
+        type:String,
+        required:true,
     },
-    image: {
-      type: Object,
-      required: true,
+    image:{
+        type:Object,
+        required:true,
     },
-    status: {
-      type: String,
-      default: "Active",
-      enum: ["Active", "Inactive"],
+    status:{
+        type:String,
+        default:'Active',
+        enum:['Active','Inactive'],
     },
-    categoryId: { type: Types.ObjectId, ref: "Category", required: true },
-    createdBy: { type: Types.ObjectId, ref: "User" },
-    updatedBy: { type: Types.ObjectId, ref: "User" },
-  },
-  {
-    timestamps: true,
-  }
+    categoryId:{type:Types.ObjectId,ref:'Category',required:true},
+    createdBy:{type:Types.ObjectId,ref:'User'},
+    updatedBy:{type:Types.ObjectId,ref:'User'},
+},
+{
+    timestamps:true,}
 );
-const subcategoryModel =
-  mongoose.models.Subcategory || model("Subcategory", subcategorySchema);
+const subcategoryModel = mongoose.models.Subcategory ||  model('Subcategory',subcategorySchema);
 export default subcategoryModel;
